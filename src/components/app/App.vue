dc7dc672afae98df6b3377fae7c59fa171980d01
Khalim0v <mrkhalimov10@gmail.com>
2023-02-18 16:49:57 +0500

    added Post and Delete

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -57,8 +57,13 @@ export default {
     }
   },
   methods: {
-    createMovie(item) {
-      this.movies.push(item)
+    async createMovie(item) {
+      try {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', item) //serverga post joylash
+        this.movies.push(response.data)
+      } catch (error) {
+        alert(error.message)
+      }
     },
     onToggleHandler({ id, prop }) {
       this.movies = this.movies.map(item => {
@@ -68,8 +73,13 @@ export default {
         return item
       })
     },
-    onRemoveHandler(id) {
-      this.movies = this.movies.filter(c => c.id != id)
+    async onRemoveHandler(id) {
+      try {
+        const response = await axios.delete('https://jsonplaceholder.typicode.com/posts/${id}')
+        this.movies = this.movies.filter(c => c.id != id)
+      } catch (error) {
+        alert(error.message)
+      }
     },
     onSearchHandler(arr, term) { //qidiruv tizmiga ishlatiluvchi funksiya
       if (term.length == 0) {
